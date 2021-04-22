13d3b0f1755d985794636185b787adb7d3671dca
Vincent <vincecreton@hotmail.fr>
2021-04-22 22:01:14 +0200

    🧩 Add AvatarList component for avatar selection

---
+++ components/AvatarList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="avatar-container">
+    <div
+      v-for="avatar in avatarArray"
+      :key="avatar.name"
+      class="avatar"
+      @click="makeAvatarCurrent($event)"
+    >
+      <img :src="avatar.avatarUrl" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatarList',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    avatarArray: {
+      type: Array,
+    },
+  },
+  methods: {
+    makeAvatarCurrent: (event) => {
+      document.querySelectorAll('.avatar').forEach((element) => {
+        element.classList.remove('focus')
+      })
+      event.path[1].classList.toggle('focus')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.avatar-container {
+  margin-left: 10px;
+  margin-top: 10px;
+}
+.avatar {
+  height: 55px;
+  width: 55px;
+  margin-bottom: 10px;
+  border-radius: 50%;
+  border: #13538b 2px solid;
+  cursor: pointer;
+  transition: all 0.1s;
+  overflow: hidden;
+}
+.avatar img {
+  object-fit: cover;
+  object-position: center;
+  height: 55px;
+  width: 55px;
+}
+.avatar.focus {
+  border: #2583db 4px solid;
+}
+</style>
--- components/MapOverlay.vue
+++ components/MapOverlay.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>Ici ce serras l'overlay</div>
+  <div>
+    <AvatarList :avatar-array="avatars"></AvatarList>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'MapOverlay',
+  data: () => {
+    return {
+      avatars: [
+        {
+          name: 'Vincent',
+          avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+        },
+        {
+          name: 'Jojo',
+          avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+        },
+        {
+          name: 'Léo',
+          avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+        },
+        {
+          name: 'Billy',
+          avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
+        },
+      ],
+    }
+  },
 }
 </script>
 
