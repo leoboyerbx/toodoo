328c726f4b5b3d92d243b929fb817e3dd6924280
Vincent <vincecreton@hotmail.fr>
2021-04-30 13:27:49 +0200

    🚧 Add get game to game the game in the store

--- mobile-app/store/apiService.js
+++ mobile-app/store/apiService.js
@@ -2,20 +2,28 @@ import axios from 'axios'
 
 export const state = () => ({
   players: [],
+  game: null,
 })
 
 export const mutations = {
   SET_PLAYERS(state, players) {
     state.players = players
   },
+  SET_GAME(state, game) {
+    state.game = game
+  },
 }
 
 export const getters = {}
 
 export const actions = {
+  getGame({commit}) {
+    axios.get('http://localhost:7554/games/2').then((response) => {
+      commit('SET_GAME', response.data)
+    })
+  },
   getPlayers({ commit }) {
     axios.get('http://localhost:7554/players').then((response) => {
-      console.log(response.data)
       commit('SET_PLAYERS', response.data)
     })
   },
