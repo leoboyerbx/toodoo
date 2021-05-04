afb718254d975492d01fdc3e879d4b8e64e6515b
thejojo974 <johanndumoulin734@gmail.com>
2021-05-04 15:35:38 +0200

    🧩 Add MissionPin component

---
+++ mobile-app/components/MissionPin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Pin :position="position" @open="$emit('open', $event)">
+      <p class="text-center">{{ mission.name }}</p>
+    </Pin>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MissionPin',
+  props: {
+    position: {
+      type: Object,
+      required: true,
+    },
+    mission: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
