03adbcdfc362d486adfa79323b476939e7c00bef
thejojo974 <johanndumoulin734@gmail.com>
2021-05-04 16:57:18 +0200

    🧩 Add Button on pin popup

--- mobile-app/components/MissionPin.vue
+++ mobile-app/components/MissionPin.vue
@@ -2,6 +2,9 @@
   <div>
     <Pin :position="position" @open="$emit('open', $event)">
       <p class="text-center">{{ mission.name }}</p>
+      <button class="flex mx-auto mt-6 rounded-full p-2 bg-theme">
+        <unicon name="check" fill="#fff" />
+      </button>
     </Pin>
   </div>
 </template>
