b7db2d1950e86c7518dd0d6cbad2f7ec971fd45a
Vincent <vincecreton@hotmail.fr>
2021-05-06 09:53:25 +0200

    🧩 Add ponctualMission component

---
+++ mobile-app/components/PonctualMissionLine.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>Passer l'aspirateur</div>
+    <div>Fait</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PonctualMissionLine',
+}
+</script>
+
+<style scoped></style>
