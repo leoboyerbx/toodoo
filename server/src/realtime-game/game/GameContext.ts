2f23bd9f7e795cdae21ef8df8299e8b45f5db9fd
leoboyerbx <contact@leoboyer.fr>
2021-05-06 14:31:46 +0200

    🧩 Who's turn in game context

--- server/src/realtime-game/game/GameContext.ts
+++ server/src/realtime-game/game/GameContext.ts
@@ -3,14 +3,19 @@ import prismaClient from "../../database/prismaClient";
 import BossEntity from "../entities/BossEntity";
 import PlayerEntity from "../entities/PlayerEntity";
 import Common from "../../helpers/Common";
+import Entity from "../entities/Entity";
 
 export default class GameContext {
   public game: Game;
   public boss: BossEntity;
   public players: PlayerEntity[];
-  public week: Number;
+  public week: number;
 
-  public async setGame(gameId: Number) {
+  // Index of who's turn. If -1, its boss turn
+  public turnIndex?: number;
+  public turnEntity?: Entity;
+
+  public async setGame(gameId: number) {
     this.game = await prismaClient.game.findFirst({
       where: {
         id: Number(gameId),
@@ -35,6 +40,21 @@ export default class GameContext {
     for (const player of players) {
       const avatar = await Common.getAvatar(player.avatar);
       this.players.push(new PlayerEntity(avatar, player));
+    }
+  }
+  setTurn(index: number) {
+    this.turnIndex = index;
+    if (index === -1) {
+      this.turnEntity = this.boss;
+    } else {
+      this.turnEntity = this.players[index];
+    }
+  }
+  nextTurn() {
+    if (this.turnIndex >= this.players.length - 1) {
+      this.setTurn(-1);
+    } else {
+      this.setTurn(this.turnIndex + 1);
     }
   }
 }
