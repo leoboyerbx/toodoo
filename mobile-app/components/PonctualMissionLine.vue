c8f9fccdbc606fe07b4ecc0a8db36bea9bb63376
Vincent <vincecreton@hotmail.fr>
2021-05-06 11:19:32 +0200

    🚧 Make the PonctualMissionsList dynamic

--- mobile-app/components/PonctualMissionLine.vue
+++ mobile-app/components/PonctualMissionLine.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
-    <div>Passer l'aspirateur</div>
-    <div>Fait</div>
+    <div>{{ mission.name }}</div>
+    <div>{{ completed ? 'Terminée' : 'Fait' }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'PonctualMissionLine',
+  props: {
+    mission: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => {
+    return {
+      completed: false,
+    }
+  },
+  mounted() {
+    this.mission.missionCompletion.forEach((completion) => {
+      const completionDate = new Date(completion.completeDay)
+      const currentDate = new Date()
+      if (
+        completionDate.getDate() === currentDate.getDate() &&
+        completionDate.getFullYear() === currentDate.getFullYear()
+      ) {
+        this.completed = true
+      }
+    })
+  },
 }
 </script>
 
