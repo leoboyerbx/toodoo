4e39889a7c9aaed8304e3e4e58c2d1d6ae8bc94a
leoboyerbx <contact@leoboyer.fr>
2021-05-06 17:08:32 +0200

    🏄 Game controller capabilities layout

--- mobile-app/components/boss/fight/CapabilityButton.vue
+++ mobile-app/components/boss/fight/CapabilityButton.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="bg-white rounded-lg flex">
-    {{ capability.name }}
+  <div class="bg-white text-theme-dark px-6 py-4 w-72 rounded-lg flex flex-col">
+    <div class="font-bold">
+      {{ capability.name }}
+    </div>
+    <div class="flex justify-between">
+      <div>Coût: {{ capability.cost }} énergie</div>
+      <div v-if="effect.attack">Dégâts: {{ effect.attack }}</div>
+      <div v-if="effect.heal">Soigne {{ effect.heal }} PV</div>
+    </div>
   </div>
 </template>
 <script>
@@ -9,6 +16,11 @@ export default {
     capability: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    effect() {
+      return this.capability.effect
     },
   },
 }
--- mobile-app/components/boss/fight/Controller.vue
+++ mobile-app/components/boss/fight/Controller.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="w-full h-full flex items-center justify-center">
-    <section class="max-w-lg flex flex-wrap">
+    <section class="max-w-2xl flex flex-wrap gap-4">
       <CapabilityButton
         v-for="capability in capabilities"
         :key="capability.name"
         :capability="capability"
+        @click.native="useCapability(capability)"
       />
     </section>
   </div>
@@ -17,6 +18,11 @@ export default {
     capabilities: {
       type: Array,
       default: () => [],
+    },
+  },
+  methods: {
+    useCapability(capability) {
+      console.log(capability)
     },
   },
 }
