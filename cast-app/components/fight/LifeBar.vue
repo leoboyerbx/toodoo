ec73e40aa8b23602636b80d25fc4dd45aced298e
leoboyerbx <contact@leoboyer.fr>
2021-05-07 10:43:39 +0200

    🧩 Created Lifebar component

---
+++ cast-app/components/fight/LifeBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="flex flex-col w-96 text-md text-white"
+    :class="align === 'left' ? 'items-start' : 'items-end'"
+  >
+    <div class="text-lg mb-5">
+      {{ name }}
+    </div>
+    <div class="relative w-full h-4 bg-gray-200 rounded overflow-hidden">
+      <div
+        class="absolute w-full top-0 left-0 w-full h-full transition-transform duration-300 bg-theme-light"
+        :style="{
+          transform: `scaleX(${entity.hp / entity.initialHp})`,
+          transformOrigin: `${align}`,
+        }"
+      ></div>
+    </div>
+    <div class="mt-4">{{ entity.hp }} / {{ entity.initialHp }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    entity: {
+      type: Object,
+      required: true,
+    },
+    align: {
+      type: String,
+      default: 'left',
+      validator: (value) => ['left', 'right'].includes(value),
+    },
+  },
+}
+</script>
