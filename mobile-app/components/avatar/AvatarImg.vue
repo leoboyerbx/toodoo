cd0071fd714bad1eccba24c93047ae53ef720e20
leoboyerbx <contact@leoboyer.fr>
2021-05-07 15:33:37 +0200

    🧩 AvatarImg component

---
+++ mobile-app/components/avatar/AvatarImg.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    v-if="avatar"
+    :src="require('assets/img/avatars/' + avatar.img[avatarType])"
+    alt="avatar"
+  />
+</template>
+<script>
+import { getAvatar } from 'common/entities/getEntity'
+
+export default {
+  props: {
+    avatarName: {
+      type: String,
+      required: true,
+    },
+    avatarType: {
+      type: String,
+      default: 'character',
+    },
+  },
+  data() {
+    return {
+      avatar: null,
+    }
+  },
+  beforeMount() {
+    this.loadAvatar()
+  },
+  beforeUpdate() {
+    this.loadAvatar()
+  },
+  methods: {
+    async loadAvatar() {
+      this.avatar = await getAvatar(this.avatarName)
+    },
+  },
+}
+</script>
