979b8a3c9c53a8360874ca4a1f5b4efa95197261
leoboyerbx <contact@leoboyer.fr>
2021-05-11 09:10:34 +0200

    🔧 Fix app name in config

--- mobile-app/nuxt.config.js
+++ mobile-app/nuxt.config.js
@@ -38,7 +38,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'gobelins-vert',
+    title: 'Toodoo App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
