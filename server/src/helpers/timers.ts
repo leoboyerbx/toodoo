02e0fb3b3ce16dd50d0463eb9a03b81b9505e0bc
leoboyerbx <contact@leoboyer.fr>
2021-05-18 21:14:13 +0200

    💡 Commenting timers.ts

--- server/src/helpers/timers.ts
+++ server/src/helpers/timers.ts
@@ -1,3 +1,7 @@
+/**
+ * Promise based timer function to use with await
+ * @param delay
+ */
 export function delay(delay: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, delay);
