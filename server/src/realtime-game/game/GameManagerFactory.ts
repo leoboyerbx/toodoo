15f6bc343b51f334eeacc2d2588d0e128915c4a2
leoboyerbx <contact@leoboyer.fr>
2021-05-18 21:46:32 +0200

    💡 Commenting GameManagerFactory.ts

--- server/src/realtime-game/game/GameManagerFactory.ts
+++ server/src/realtime-game/game/GameManagerFactory.ts
@@ -1,22 +1,32 @@
-import GameManager from './GameManager';
+import GameManager from "./GameManager";
 
+/**
+ * Class used to generate Game manager instances, in order to link socket clients with same ID to the same GameManager
+ */
 export default class GameManagerFactory {
   private static instance: GameManagerFactory;
-  private gameInstances: Array<GameManager> = []
+  private gameInstances: Array<GameManager> = [];
 
+  /**
+   * Returns a game corresponding to the provided id, creating it if doesn't exist
+   * @param id
+   */
   getGameWithId(id: string): GameManager {
     const existingGame = this.gameInstances.find((game: GameManager) => {
-      return game.id === id
-    })
+      return game.id === id;
+    });
     if (existingGame) {
-      return existingGame
+      return existingGame;
     } else {
-      const newGame = new GameManager(id)
-      this.gameInstances.push(newGame)
-      return newGame
+      const newGame = new GameManager(id);
+      this.gameInstances.push(newGame);
+      return newGame;
     }
   }
 
+  /**
+   * Get class singleton instance
+   */
   public static getInstance(): GameManagerFactory {
     if (!GameManagerFactory.instance) {
       GameManagerFactory.instance = new GameManagerFactory();
