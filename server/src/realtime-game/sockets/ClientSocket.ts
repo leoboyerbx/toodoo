b498a2b424f4be550118852fbe21afd8577629f5
leoboyerbx <contact@leoboyer.fr>
2021-05-18 21:51:01 +0200

    💡 Commenting ClientSocket.ts

--- server/src/realtime-game/sockets/ClientSocket.ts
+++ server/src/realtime-game/sockets/ClientSocket.ts
@@ -2,6 +2,9 @@ import { Socket } from "socket.io";
 import PairDataInterface, { ClientType } from "./PairDataInterface";
 import GameManagerFactory from "../game/GameManagerFactory";
 
+/**
+ * Class wrapping a Socket.io socket, with pairing properties
+ */
 export default class ClientSocket {
   get clientType(): ClientType {
     return this._clientType;
@@ -14,17 +17,38 @@ export default class ClientSocket {
     socket.emit("start");
     this.bindSocket();
   }
+
+  /**
+   * Bind socket events
+   */
   bindSocket() {
     this.socket.on("pair", this.pair.bind(this));
   }
+
+  /**
+   * Pair the socket with the game manager with specified ID
+   * @param data
+   */
   pair(data: PairDataInterface) {
     this._clientType = data.type;
     const gameManager = GameManagerFactory.getInstance().getGameWithId(data.id);
     gameManager.join(this);
   }
+
+  /**
+   * Pass-through to Socket.io emit() method
+   * @param event
+   * @param payload
+   */
   emit(event, payload?) {
     return this.socket.emit(event, payload);
   }
+
+  /**
+   * Pass-through to Socket.io on() method
+   * @param event
+   * @param listener
+   */
   on(event, listener?) {
     return this.socket.on(event, listener);
   }
