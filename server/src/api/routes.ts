984b33a5f2dda5ef21000a37038b055f33a10e77
Vincent <vincecreton@hotmail.fr>
2021-05-22 16:02:40 +0200

    📚 Comment API

--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -12,11 +12,20 @@ const jsonParser = bodyParser.json();
  * @param app
  */
 export default function setupRoutes(app: Express): void {
+  /**
+   * Get all games
+   * @route "/games"
+   */
   app.get("/games", async (req, res) => {
     const games = await prisma.game.findMany({});
     res.json(games);
   });
 
+  /**
+   * Get a game with a his ID
+   * @route "/games/<id>"
+   * @param id
+   */
   app.get("/games/:id", async (req, res) => {
     const { id } = req.params;
     const games = await prisma.game.findMany({
@@ -27,6 +36,10 @@ export default function setupRoutes(app: Express): void {
     res.json(games);
   });
 
+  /**
+   * Get all teams
+   * @route "/teams"
+   */
   app.get("/teams", async (req, res) => {
     const teams = await prisma.team.findMany({
       include: {
@@ -36,6 +49,11 @@ export default function setupRoutes(app: Express): void {
     res.json(teams);
   });
 
+  /**
+   * Get a team with a his ID
+   * @route "/teams/<id>"
+   * @param id
+   */
   app.get("/teams/:id", async (req, res) => {
     const { id } = req.params;
     const team = await prisma.team.findUnique({
@@ -50,11 +68,20 @@ export default function setupRoutes(app: Express): void {
     res.json(team);
   });
 
+  /**
+   * Get all players
+   * @route "/players"
+   */
   app.get("/players", async (req, res) => {
     const players = await prisma.player.findMany();
     res.json(players);
   });
 
+  /**
+   * Get a player with his ID
+   * @route "/players/<id>"
+   * @param id
+   */
   app.get("/players/:id", async (req, res) => {
     const { id } = req.params;
     const player = await prisma.player.findUnique({
@@ -69,6 +96,11 @@ export default function setupRoutes(app: Express): void {
     res.json(player);
   });
 
+  /**
+   * Get all players of a team with the team ID
+   * @route "/players/team/<id>"
+   * @param id
+   */
   app.get("/players/team/:teamId", async (req, res) => {
     const { teamId } = req.params;
     const players = await prisma.player.findMany({
@@ -83,11 +115,20 @@ export default function setupRoutes(app: Express): void {
     res.json(players);
   });
 
+  /**
+   * Get all missions
+   * @route "/missions"
+   */
   app.get("/missions", async (req, res) => {
     const missions = await prisma.mission.findMany();
     res.json(missions);
   });
 
+  /**
+   * Get a mission with his ID
+   * @route "/missions/<id>"
+   * @param id
+   */
   app.get("/missions/:id", async (req, res) => {
     const { id } = req.params;
     const missions = await prisma.mission.findMany({
@@ -98,6 +139,11 @@ export default function setupRoutes(app: Express): void {
     res.json(missions);
   });
 
+  /**
+   * Get all missions of a game with the game ID
+   * @route "/missions/game/<id>"
+   * @param id
+   */
   app.get("/missions/game/:gameId", async (req, res) => {
     const { gameId } = req.params;
     const missions = await prisma.mission.findMany({
@@ -112,6 +158,11 @@ export default function setupRoutes(app: Express): void {
     res.json(missions);
   });
 
+  /**
+   * Get all missions of a player with the player ID
+   * @route "/missions/player/<id>"
+   * @param id
+   */
   app.get("/missions/player/:playerId", async (req, res) => {
     const { playerId } = req.params;
     const missions = await prisma.mission.findMany({
@@ -122,6 +173,10 @@ export default function setupRoutes(app: Express): void {
     res.json(missions);
   });
 
+  /**
+   * Post a new mission completion
+   * @param missionId, completeBy, completeDay
+   */
   app.post(`/mission-completion`, jsonParser, async (req, res) => {
     const { missionId, completeBy, completeDay, teamId, gameId } = req.body;
     const result = await prisma.missionCompletion.create({
