3dadd0bed87d347d1a53f5c73ef4dc712ea0848e
Vincent <vincecreton@hotmail.fr>
2021-06-08 09:11:41 +0200

    🍱 Add unicon uniChartGrowthAlt

--- mobile-app/plugins/vue-unicons.js
+++ mobile-app/plugins/vue-unicons.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Unicon from 'vue-unicons/dist/vue-unicons-vue2.es'
-import { uniArrowLeft, uniCheck, uniSchedule } from 'vue-unicons/dist/icons'
+import {
+  uniArrowLeft,
+  uniCheck,
+  uniSchedule,
+  uniChartGrowthAlt,
+} from 'vue-unicons/dist/icons'
 
-Unicon.add([uniCheck, uniArrowLeft, uniSchedule])
+Unicon.add([uniCheck, uniArrowLeft, uniSchedule, uniChartGrowthAlt])
 Vue.use(Unicon)
