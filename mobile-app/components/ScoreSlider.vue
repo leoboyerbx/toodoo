1f27cc459599660f5cc66a423501a5ce43c3c19c
Vincent <vincecreton@hotmail.fr>
2021-06-08 14:45:02 +0200

    🧩 Add score page and Score Slider component

---
+++ mobile-app/components/ScoreSlider.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: 'ScoreSlider',
+}
+</script>
+
+<style scoped></style>
---
+++ mobile-app/pages/score.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fullscreen bg-theme-darkBlue gradient-bg">
+    <button
+      class="absolute top-1.5 left-10 flex mx-auto mt-6 rounded-full p-2 bg-white z-10"
+      @click="$router.go(-1)"
+    >
+      <unicon name="arrow-left" fill="#b5b1fe" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.gradient-bg {
+  background: linear-gradient(
+    137deg,
+    rgba(72, 56, 141, 1) 40%,
+    rgba(172, 168, 241, 1) 100%
+  );
+}
+</style>
