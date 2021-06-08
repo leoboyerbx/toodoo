dcc0ca8a98cf2780c21f6e7239b8f38abfc7a44c
Vincent <vincecreton@hotmail.fr>
2021-06-08 17:43:44 +0200

    🏄‍ Complete missions list layout

--- mobile-app/components/ScoreCompletionLine.vue
+++ mobile-app/components/ScoreCompletionLine.vue
@@ -1,10 +1,21 @@
 <template>
-  <div></div>
+  <div class="bg-white rounded-md mt-6 pt-2 pb-2 pl-4 pr-4 flex-nowrap flex">
+    <span class="w-4/6 font-body text-black font-light">
+      {{ mission.mission.name }}</span
+    >
+    <span class="w-2/6 font-body text-black font-light"> 12 énergies </span>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ScoreCompletionLine',
+  props: {
+    mission: {
+      type: Object,
+      required: true,
+    },
+  },
 }
 </script>
 
