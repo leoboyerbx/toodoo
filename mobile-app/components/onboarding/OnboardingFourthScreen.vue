e597458014fc1a0ea892573588674ef1ba216a74
Vincent <vincecreton@hotmail.fr>
2021-06-09 16:42:50 +0200

    🏄‍ Style of OnboardingFourthScreen.vue

---
+++ mobile-app/components/onboarding/OnboardingFourthScreen.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fullscreen">
+    <img
+      class="h-screen w-screen object-cover object-center"
+      :src="require('assets/img/assets-onboarding/screen4/ecran_4.svg')"
+      alt=""
+    />
+    <img
+      class="vinou absolute"
+      :src="require('assets/img/assets-onboarding/screen4/vinou.svg')"
+      alt=""
+    />
+    <img
+      class="boun absolute"
+      :src="require('assets/img/assets-onboarding/screen4/boun.svg')"
+      alt=""
+    />
+    <img
+      class="lyo absolute"
+      :src="require('assets/img/assets-onboarding/screen4/lyo.svg')"
+      alt=""
+    />
+    <img
+      class="maylo absolute"
+      :src="require('assets/img/assets-onboarding/screen4/maylo.svg')"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OnboardingFourthScreen',
+}
+</script>
+
+<style scoped>
+.vinou {
+  left: 30px;
+  bottom: 20vh;
+}
+.boun {
+  left: 110px;
+  bottom: 17vh;
+}
+.lyo {
+  left: -25px;
+  bottom: 18vh;
+}
+.maylo {
+  left: -70px;
+  bottom: 15vh;
+}
+</style>
