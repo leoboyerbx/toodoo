efe267c97b24782c1c8c396db75009f60ef5cb2b
Vincent <vincecreton@hotmail.fr>
2021-06-09 13:39:31 +0200

    🥷 Update component OnboardingTextBox.vue

--- mobile-app/components/OnboardingTextBox.vue
+++ mobile-app/components/OnboardingTextBox.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="flex flex-nowrap w-4/6 bg-white rounded-2xl p-8">
+  <div class="flex flex-nowrap w-4/6 bg-white rounded-2xl p-8 items-center">
     <div class="text-content w-5/6">{{ textToDisplay }}</div>
     <div class="w-1/6">
-      <button class="flex mx-auto rounded-full bg-theme p-3">
+      <button
+        class="flex mx-auto rounded-full bg-theme p-3"
+        @click="emmitClick"
+      >
         <unicon name="arrow-right" fill="#fff" />
       </button>
     </div>
@@ -16,6 +19,14 @@ export default {
     textToDisplay: {
       type: String,
       required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    emmitClick() {
+      this.$emit('increment-counter')
     },
   },
 }
