6db3024205dede117350eea9be2bbce8f3a6ba0b
Vincent <vincecreton@hotmail.fr>
2021-06-09 11:23:01 +0200

    🍱 Add arrow right unicon

--- mobile-app/plugins/vue-unicons.js
+++ mobile-app/plugins/vue-unicons.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import Unicon from 'vue-unicons/dist/vue-unicons-vue2.es'
 import {
   uniArrowLeft,
+  uniArrowRight,
   uniCheck,
   uniSchedule,
   uniChartGrowthAlt,
 } from 'vue-unicons/dist/icons'
 
-Unicon.add([uniCheck, uniArrowLeft, uniSchedule, uniChartGrowthAlt])
+Unicon.add([
+  uniCheck,
+  uniArrowLeft,
+  uniSchedule,
+  uniChartGrowthAlt,
+  uniArrowRight,
+])
 Vue.use(Unicon)
