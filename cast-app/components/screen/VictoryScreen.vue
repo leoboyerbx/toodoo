b8d5710c715a614ed2b1ee05fa3bdf96b5d094bd
leoboyerbx <contact@leoboyer.fr>
2021-06-09 13:23:28 +0200

    🏄 Victory screen

--- cast-app/components/screen/VictoryScreen.vue
+++ cast-app/components/screen/VictoryScreen.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <div class="w-full h-full relative text-white">
-      <p v-if="gameContext.winner === 'boss'">Le boss a gagné !</p>
-      <p v-else>Bravo ! Vous avez battu le boss !</p>
-      <!--      <img-->
-      <!--        :src="require(`assets/img/boss/${boss.img.decor}`)"-->
-      <!--        alt=""-->
-      <!--        class="absolute top-0 left-0 w-full h-full object-cover"-->
-      <!--      />-->
-      <!--      <span-->
-      <!--        class="absolute top-10 left-0 w-full flex justify-center text-xl font-bold"-->
-      <!--      >-->
-      <!--        {{ boss.name }}-->
-      <!--      </span>-->
-      <!--      <img-->
-      <!--        :src="require(`assets/img/boss/${boss.img.character}`)"-->
-      <!--        alt="Boss image"-->
-      <!--        class="absolute w-64"-->
-      <!--        :style="{-->
-      <!--          left: boss.img.positionInDecor.x + '%',-->
-      <!--          top: boss.img.positionInDecor.y + '%',-->
-      <!--        }"-->
-      <!--      />-->
-      <!--      <div class="absolute left-0 bottom-0 w-1/2 h-1/4 flex">-->
-      <!--        <div-->
-      <!--          v-for="player in players"-->
-      <!--          :key="player.player.id"-->
-      <!--          class="flex flex-col mx-5 justify-end items-center"-->
-      <!--        >-->
-      <!--          <img-->
-      <!--            :src="require(`assets/img/avatars/${player.img.character}`)"-->
-      <!--            alt="Avatar image"-->
-      <!--            class="h-full"-->
-      <!--          />-->
-      <!--        </div>-->
-      <!--      </div>-->
+  <div
+    class="w-full h-full font-display flex flex-col justify-center items-center text-white"
+  >
+    <img
+      :src="require(`assets/img/boss/${boss.img.decor}`)"
+      alt=""
+      class="absolute top-0 left-0 w-full h-full object-cover z-0"
+    />
+    <h1 class="text-4xl z-10 mt-5">Victoire !</h1>
+    <p
+      class="font-sans z-10 w-1/2 text-lg my-6 text-center"
+      style="line-height: 1.2"
+    >
+      Grâce à cette victoire vous gagnez une chance supplémentaire de battre le
+      Chef des Electros !
+    </p>
+    <img src="~assets/img/heart.png" alt="" class="z-10 w-28" />
+
+    <div class="flex z-10 justify-center">
+      <div
+        v-for="player in players"
+        :key="player.player.id"
+        class="flex flex-col -mx-10 justify-end items-center"
+      >
+        <img
+          :src="require(`assets/img/avatars/${player.img.character}`)"
+          alt="Avatar image"
+          class="w-64"
+        />
+      </div>
     </div>
   </div>
 </template>
