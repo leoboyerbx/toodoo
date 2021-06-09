b5cf2dff675e61ad6cbb230ab46aac6d479032b1
Vincent <vincecreton@hotmail.fr>
2021-06-09 20:09:29 +0200

    🥷 Save teamName input before query

--- mobile-app/pages/onboarding/teamName.vue
+++ mobile-app/pages/onboarding/teamName.vue
@@ -3,13 +3,14 @@
     <div class="content w-screen max-w-2xl mx-auto">
       <div class="title text-white font-display text-2xl">Nom de la team</div>
       <input
-        v-model="newMissionName"
+        v-model="newTeamName"
         class="max-w-3xl w-full mt-5 flex flex-row flex-nowrap justify-between rounded-lg bg-white pt-2.5 pb-1 pl-3 border-pink h-12"
         placeholder="Le nom de votre équipe..."
       />
       <div class="buttton-line justify-end flex mt-8">
         <div
           class="button bg-pink text-white font-display rounded-3xl text-2xl pt-4 pb-4 pr-7 pl-7 w-48 text-center"
+          @click="saveNameTeam"
         >
           Valider
         </div>
@@ -21,6 +22,19 @@
 <script>
 export default {
   name: 'TeamName',
+  data() {
+    return {
+      newTeamName: '',
+    }
+  },
+  methods: {
+    saveNameTeam() {
+      this.$store.dispatch('apiService/setTeam', {
+        team: { name: this.newTeamName },
+      })
+      this.$router.push('/onboarding/addPlayer')
+    },
+  },
 }
 </script>
 
