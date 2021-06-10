041967f02e7fced916ac7f947922369c5ad7c9ab
leoboyerbx <contact@leoboyer.fr>
2021-06-10 22:24:36 +0200

    Update user scalable

--- mobile-app/nuxt.config.js
+++ mobile-app/nuxt.config.js
@@ -41,7 +41,10 @@ export default {
     title: 'Toodoo App',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable="no"',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
