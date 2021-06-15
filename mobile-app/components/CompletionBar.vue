b67c5292a2ae9aa86c32be889484df256976b56b
Vincent <vincecreton@hotmail.fr>
2021-06-15 10:12:37 +0200

    🧩 Add CompletionBar.vue component

---
+++ mobile-app/components/CompletionBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="dot" :v-for="n in 7"></div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompletionBar',
+}
+</script>
+
+<style scoped>
+.line {
+  width: 605px;
+  height: 1px;
+  background-color: #ffffff;
+}
+</style>
