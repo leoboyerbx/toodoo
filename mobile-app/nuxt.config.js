26e82460f37c46f54804cba8bb6fb99541a8930f
leoboyerbx <contact@leoboyer.fr>
2021-06-15 10:44:47 +0200

    🧩 No orphans component

--- mobile-app/nuxt.config.js
+++ mobile-app/nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, user-scalable="no"',
+        content: 'width=device-width, initial-scale=1, user-scalable=no',
       },
       { hid: 'description', name: 'description', content: '' },
     ],
